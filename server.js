a20b5f2b64431ec414add0b1b1f9ea8d0e58b371
b216014 <b216014@iiit-bh.ac.in>
2020-06-29 20:08:09 +0530

    Added a graphQL server and a json server to handle/serve api requests.

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const expressGraphQL = require('express-graphql');
+const schema = require('./schema/schema');
 
 const app = express();
+
+app.use('/graphql', expressGraphQL({
+    schema,
+    // A development tool that allows us to make queries against our development server. Only intended to use in a development environment
+    graphiql: true 
+}))
 
 app.listen(4000, () => {
     console.log('Listening to port 4000!')
